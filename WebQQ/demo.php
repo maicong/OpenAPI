9c6a30a05b40b647aa1f2dee9c562db2346cc5d6
Maicong <i@yuxiaoxi.com>
2015-01-21 20:04:07 +0800

    新增运行状态显示【注：请在终端(Terminal)中运行】

--- WebQQ/demo.php
+++ WebQQ/demo.php
@@ -1,35 +1,29 @@
+#!/usr/bin/php
 <?php
-/**
- *
- * PHP WEB QQ [演示]
- *
- * @author     MaiCong <admin@maicong.me>
- * @date  2015-01-21 16:52:17
- * @package    webqq
- * @version    0.1.0 alpha
- *
- */
-
+if (php_sapi_name() != "cli") exit ("请在终端(Terminal)中运行! \n");
 header('Content-Type:text/html; charset=utf-8');
+define('C_PATH', rtrim(str_replace("\\", "/", dirname(__FILE__)), '/'));
 require_once 'webqq.class.php';
+echo "--------> 登录中 \n";
 
 $uin = '12345678'; // 帐号
 $pwd = '12345678'; // 密码
 
-$webqq = new PHPWebQQ($uin, $pwd);
+$webqq = new WebQQ($uin, $pwd);
 $cookie = $webqq->get_cookie();
 $cookie['clientid'] = mt_rand(20000000,88888888);
 if(empty($cookie['ptwebqq'])){
     if($webqq->qq_login()){
         $cookie = $webqq->get_cookie();
+        echo "--------> 登录成功 \n";
     }
 }else{
     if(empty($cookie['psessionid'])){
         $online = $webqq->qq_online($cookie['ptwebqq'], $cookie['clientid']);
         $cookie += $online;
+        echo "--------> 上线成功 \n";
     }
 }
-// 抓取内容
 function http_get($url){
     $header = array(
         'Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
@@ -51,7 +45,7 @@ function http_get($url){
     curl_close($ch);
     return $response;
 }
-// object转array
+// object转arrat
 function object2array($obj){
     $_arr = is_object($obj)?get_object_vars($obj):$obj;
     foreach ($_arr as $key => $val){
@@ -61,15 +55,14 @@ function object2array($obj){
     return $arr; 
 }
 // 清除HTML标记
-function clear_html($str) {
+function clearhtml($str) {
     $str = str_replace(array('&nbsp;', '&amp;', '&quot;', '&#039;', '&ldquo;', '&rdquo;', '&mdash;', '&lt;', '&gt;', '&middot;', '&hellip;'), array(' ', '&', '"', "'", '“', '”', '—', '<', '>', '·', '…'), $str);
     $str = preg_replace("/\<[a-z]+(.*)\>/iU", "", $str);
     $str = preg_replace("/\<\/[a-z]+\>/iU", "", $str);
     $str = str_replace(array(' ','  ', chr(13), chr(10), '&nbsp;'), array('', '', '', '', ''), $str);
     return $str;
 }
-//自动回复
-function auto_reply($str){
+function zdhf($str){
     $tlinfo = http_get('http://www.tuling123.com/openapi/productexp.do?info='.$str);
     $answer = simplexml_load_string($tlinfo, 'SimpleXMLElement', LIBXML_NOCDATA);
     switch ($answer->MsgType) {
@@ -94,24 +87,8 @@ function auto_reply($str){
     $anscont = str_replace('机器人','麦葱酱',$anscont);
     $anscont = str_replace('小黄鸡','麦葱酱',$anscont);
     $anscont = str_replace('图灵','麦葱酱',$anscont);
-    return clear_html($anscont);
+    return clearhtml($anscont);
 }
-
-while(true){
-    sleep(3); // 3秒睡死撒过
-    $poll = $webqq->poll($cookie['ptwebqq'], $cookie['clientid'], $cookie['psessionid']);
-    if(empty($poll)) continue;
-    foreach ($poll as $val) {
-        $cont = auto_reply($val['value']['content'][1]);
-        if($val['poll_type'] == 'group_message'){
-            $sendmsg = $webqq->send_qun_msg($val['value']['from_uin'], $cont, $cookie['clientid'], $cookie['psessionid']);
-        }else if($val['poll_type'] == 'message'){
-            $sendmsg = $webqq->send_buddy_msg($val['value']['from_uin'], $cont, $cookie['clientid'], $cookie['psessionid']);
-        }
-       
-    }
-}
-
 //$friend_list = $webqq->get_user_friends($uin, $cookie['ptwebqq']);
 // $fuid = $friend_list['info'][0]['uin'];
 //$qun_list = $webqq->get_group_name_list_mask($uin, $cookie['ptwebqq']);
@@ -122,20 +99,28 @@ while(true){
 // print_r($qun_list);
 // $sendmsg = $webqq->send_buddy_msg('2415586530', "哈哈哈，搞siao哦", $cookie['clientid'], $cookie['psessionid']);
 // print_r($sendmsg);
-// while(true){
-//     sleep(3); // 3秒睡死撒过
-//     $poll = $webqq->poll($cookie['ptwebqq'], $cookie['clientid'], $cookie['psessionid']);
+static $run_num = 1;
+while(true){
+    sleep(3);
+    echo "--------> 正在检测新消息 ({$run_num}) \n";
+    $poll = $webqq->poll($cookie['ptwebqq'], $cookie['clientid'], $cookie['psessionid']);
+    $run_num++;
+    if(empty($poll)) continue;
+    foreach ($poll as $val) {
+        $cont = zdhf($val['value']['content'][1]);
+        echo "-----------> 获取到新消息: {$val['value']['content'][1]} \n";
+        $webqq->write_file(C_PATH.'/logs/msg.txt', "-->".$val['value']['content'][1] . "=->".  $cont."\r\n");
+        if($val['poll_type'] == 'group_message'){
+            $sendmsg = $webqq->send_qun_msg($val['value']['from_uin'], $cont, $cookie['clientid'], $cookie['psessionid']);
+        }else if($val['poll_type'] == 'message'){
+            $sendmsg = $webqq->send_buddy_msg($val['value']['from_uin'], $cont, $cookie['clientid'], $cookie['psessionid']);
+        }
+        echo "-----------> 回复成功: {$cont} \n";
+    }
+}
 
-//     foreach ($poll as $val) {
-//         if($val['poll_type'] == 'group_message'){
-//             // if(in_array(array('麦葱'),$val['value']['content'][1])){}
-//             $sendmsg = $webqq->send_qun_msg($val['value']['from_uin'], "麦葱情报机器人已将此消息暂存，等待主人查看。", $cookie['clientid'], $cookie['psessionid']);
-//         }else if($val['poll_type'] == 'message'){
-//             $sendmsg = $webqq->send_buddy_msg($val['value']['from_uin'], "好的，麦葱情报机器人已将此消息暂存，等待主人查看。", $cookie['clientid'], $cookie['psessionid']);
-//         }
-       
-//     }
-// }
+
+
 
 // $sendmsg = $webqq->send_buddy_msg('2415586530', "哈哈哈，搞siao哦", $cookie['clientid'], $cookie['psessionid']);
 // $sendqunmsg = $webqq->send_qun_msg('2558014021', "什么情况怎么发不出去了", $cookie['clientid'], $cookie['psessionid']);
