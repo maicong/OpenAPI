cc5358edc3e4f9afdfaaa6f8edbd2f1174ee9d77
maicong <i@maicong.me>
2016-08-26 18:04:33 +0800

    fix bug

--- server/bat/start_server.bat
+++ server/bat/start_server.bat
@@ -5,5 +5,5 @@ tasklist | findstr /i nginx.exe && taskkill /f /im nginx.exe
 tasklist | findstr /i svnserve.exe && taskkill /f /im svnserve.exe
 tasklist | findstr /i start_server.exe && taskkill /f /im start_server.exe
 
-mysqld.exe | php-cgi -c C:/server/php/php.ini -d 127.0.0.1:9000 | nginx.exe -p C:/server/nginx | svnserve.exe -d -r C:\server\svn --log-file C:\server\temp\svn.log
+mysqld.exe | php-cgi.exe -c C:/server/php/php.ini -b 127.0.0.1:9000 | nginx.exe -p C:/server/nginx | svnserve.exe -d -r C:\server\svn --log-file C:\server\temp\svn.log
 exit
